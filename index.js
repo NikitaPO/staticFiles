8f7977a55bb46d6a0c4cfc06907b1d4612e70c99
NikitaPO <gigzim345@gmail.com>
2018-12-20 13:12:26 +0300

    1

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const PORT = 4321;
-const serve = require('serve-static')('public', {
-'index': ['index.html', 'index.htm']
-});
-require('http').Server((req, res) => {
-if (req.url === '/hello') res.writeHead(200) || res.end('Hello!');
-serve(req, res, () => res.writeHead(404) || res.end('not found'));
+require('http')
+.Server(({url: u}, res) => {
+res.end('Hello, world!');
 })
-.listen(PORT, () => console.log(`running ${process.pid} at port ${PORT}`));
+.listen(process.env.PORT || PORT);
